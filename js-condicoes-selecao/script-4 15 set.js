11af683a586702fbed1b9365f7d74af201d991bd
Bruno Kalel <104171372+bruno-kalel@users.noreply.github.com>
2023-09-15 09:57:21 -0300

    adicionar comentário script-4 15 set

--- js-condicoes-selecao/script-4 15 set.js
+++ js-condicoes-selecao/script-4 15 set.js
@@ -4,6 +4,8 @@ let dis_passo = parseInt(prompt('dis_passo'))
 let dis_total = pos_final-pos_inicial
 let passos = 0
 
+// tbm poderia ser let dis_total = parseInt(prompt('pos_final')) - parseInt(prompt('pos_inicial'))
+
 while (dis_total > 0)
 {
     dis_total -= dis_passo
